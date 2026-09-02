d7b51c945ad15b9d8488a72b840996b654e78f08
SkyLin8012 <fagw.sky@gmail.com>
2026-09-02 12:48:50 +0800

    20260902

---
+++ backend/src/main/java/com/example/demo/modules/game/quiz/config/quizDataInitializer.java
@@ -0,0 +1,13 @@
+package com.example.demo.modules.game.quiz.config;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class quizDataInitializer implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
